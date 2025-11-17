d8d419b2993d68c610f3cd6353f7f151130f2980
SergeyMaximenko <122869028+SergeyMaximenko@users.noreply.github.com>
2025-11-17 21:57:24 +0200

    вы
    вфывыф

--- ScheduleDisconnectLight/Program.cs
+++ ScheduleDisconnectLight/Program.cs
@@ -58,7 +58,7 @@ namespace ScheduleDisconnectLight
                 {
                     var messageTmp = new StringBuilder();
                     // Отправить сообщение об изменении графика 
-                    messageTmp.Append("⚡️<b>5Увага!</b> Новий графік <b>відсутності</b> світла\n");
+                    messageTmp.Append("⚡️<b>6Увага!</b> Новий графік <b>відсутності</b> світла\n");
                     messageTmp.Append("\n");
                     if (schedule.ParamDisconnet1 != null)
                     {
