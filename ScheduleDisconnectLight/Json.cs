f814986a8808f87a885c440057ab70ec95c81a3d
SergeyMaximenko <122869028+SergeyMaximenko@users.noreply.github.com>
2025-11-17 17:44:24 +0200

    2222

---
+++ ScheduleDisconnectLight/Json.cs
@@ -0,0 +1,292 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ScheduleDisconnectLight
+{
+
+        /// <summary>
+        /// Service for working with JSON-formatted string
+        /// </summary>
+        public class Json
+        {
+            private readonly JToken _jToken;
+
+            /// <summary>
+            /// <para>Create object for working with JSON-string</para>
+            /// <para>Example:</para>
+            /// <para><code>new JsonSerializer.Json("{...}")["Prop1"]["Prop2"].Value</code></para>
+            /// </summary>
+            /// <param name="jsonString">JSON-formatted string</param>
+            public Json(string jsonString)
+            {
+                if (string.IsNullOrEmpty(jsonString))
+                {
+                    return;
+                }
+                _jToken = JToken.Parse(jsonString);
+            }
+
+            private Json(JToken jToken)
+            {
+                _jToken = jToken;
+            }
+
+            /// <summary>
+            /// <para>Retrieve a specified property from a JSON object</para>
+            /// <para>If the value doesn't exist, no error will be thrown</para>
+            /// </summary>
+            /// <param name="propertyName">Property name</param>
+            public Json this[string propertyName]
+            {
+                get
+                {
+                    if (_jToken is JObject jObject)
+                    {
+                        // Ignore case
+                        var property = jObject.Properties()
+                                                .FirstOrDefault(p => string.Equals(p.Name, propertyName, StringComparison.OrdinalIgnoreCase));
+                        if (property != null)
+                        {
+                            return new Json(property.Value);
+                        }
+                    }
+                    // An incorrect property was specified
+                    return new Json(null);
+
+                }
+            }
+
+            /// <summary>
+            /// <para>Retrieve an array element by the specified index</para>
+            /// <para>If the index is out of range, no error will be thrown</para>
+            /// </summary>
+            /// <param name="index">Array index</param>
+            public Json this[int index]
+            {
+                get
+                {
+                    if (_jToken is JArray jArray)
+                    {
+                        if (index + 1 <= jArray.Count())
+                        {
+                            return new Json(jArray[index]);
+                        }
+
+                    }
+                    // An incorrect index was specified
+                    return new Json(null);
+                }
+            }
+
+            /// <summary>
+            /// Retrieve the number of elements in a list
+            /// </summary>
+            public int Count
+            {
+                get
+                {
+                    return _jToken.Count();
+                }
+            }
+
+
+            /// <summary>
+            /// Check if a property exists in the object
+            /// </summary>
+            public bool ContainsProperty(string propertyName)
+            {
+                return (_jToken is JObject jObject) && jObject.ContainsKey(propertyName);
+            }
+
+            /// <summary>
+            /// Type of the current value
+            /// </summary>
+            public JsonType JsonType
+            {
+                get
+                {
+                    if (_jToken is JArray)
+                    {
+                        return JsonType.JArray;
+                    }
+                    if (_jToken is JObject)
+                    {
+                        return JsonType.JObject;
+                    }
+                    return JsonType.Value;
+                }
+            }
+
+            /// <summary>
+            /// Retrieve the value as the specified type
+            /// </summary>
+            public T GetValue<T>()
+            {
+                var emptyValue = default(T);
+                if (_jToken == null)
+                {
+                    return emptyValue;
+                }
+
+                try
+                {
+                    return _jToken.ToObject<T>();
+                }
+                catch (Exception ex)
+                {
+                    var value = _jToken.Value<object>();
+                    // Log the error (if needed)
+                    throw new InvalidOperationException(string.Format(
+@"Ошибка при чтении значения из JSON
+Не удалось привести тип '{0}' к '{1}'
+Ошибка: {2}
+Значение: {3}", value?.GetType().Name, typeof(T), ex.Message, value));
+
+                }
+            }
+
+
+            /// <summary>
+            /// Value as a string
+            /// </summary>
+            public string Value => GetValue<string>();
+
+            /// <summary>
+            /// Value as an integer
+            /// </summary>
+            public int ValueInt => GetValue<int>();
+
+            /// <summary>
+            /// <para>Value as a date</para> 
+            /// <para>Supported formats:</para>
+            /// <list type="bullet">
+            ///   <item>yyyy-MM-dd           // 2024-12-31 (date only)</item>
+            ///   <item>yyyy.MM.dd           // 2024.12.31 (date only)</item>
+            ///   <item>dd.MM.yyyy           // 31.12.2024 (date only)</item>
+            ///   <item>dd-MM-yyyy           // 31-12-2024 (date only)</item>
+            ///   <item>yyyy-MM-dd HH:mm:ss  // 2024-12-31 14:30:00 (date and time)</item>
+            ///   <item>yyyy.MM.dd HH:mm:ss  // 2024.12.31 14:30:00 (date and time)</item>
+            ///   <item>dd.MM.yyyy HH:mm:ss  // 31.12.2024 14:30:00 (date and time)</item>
+            ///   <item>dd-MM-yyyy HH:mm:ss  // 31-12-2024 14:30:00 (date and time)</item>
+            ///   <item>yyyy-MM-dd HH:mm     // 2024-12-31 14:30 (date and time without seconds)</item>
+            ///   <item>yyyy.MM.dd HH:mm     // 2024.12.31 14:30 (date and time without seconds)</item>
+            ///   <item>dd.MM.yyyy HH:mm     // 31.12.2024 14:30 (date and time without seconds)</item>
+            ///   <item>dd-MM-yyyy HH:mm     // 31-12-2024 14:30 (date and time without seconds)</item>
+            /// </list>
+            /// </summary>
+            public DateTime ValueDate
+            {
+                get
+                {
+                    string[] dateTimeFormats =
+                    {
+                        "yyyy-MM-dd",          // 2024-12-31 (date only)
+						"yyyy.MM.dd",          // 2024.12.31 (date only)
+						"dd.MM.yyyy",          // 31.12.2024 (date only)
+						"dd-MM-yyyy",          // 31-12-2024 (date only)
+						"yyyy-MM-dd HH:mm:ss", // 2024-12-31 14:30:00 (date and time)
+						"yyyy.MM.dd HH:mm:ss", // 2024.12.31 14:30:00 (date and time)
+						"dd.MM.yyyy HH:mm:ss", // 31.12.2024 14:30:00 (date and time)
+						"dd-MM-yyyy HH:mm:ss", // 31-12-2024 14:30:00 (date and time)
+						"yyyy-MM-dd HH:mm",    // 2024-12-31 14:30 (date and time without seconds)
+						"yyyy.MM.dd HH:mm",    // 2024.12.31 14:30 (date and time without seconds)
+						"dd.MM.yyyy HH:mm",    // 31.12.2024 14:30 (date and time without seconds)
+						"dd-MM-yyyy HH:mm"     // 31-12-2024 14:30 (date and time without seconds)
+					};
+
+                    DateTime.TryParseExact(Value, dateTimeFormats, CultureInfo.InvariantCulture, DateTimeStyles.None, out var parsedDateTime);
+                    return parsedDateTime;
+                }
+            }
+
+            /// <summary>
+            /// Value as a decimal number
+            /// </summary>
+            public decimal ValueDecimal => GetValue<decimal>();
+
+            /// <summary>
+            /// Value as a boolean
+            /// </summary>
+            public bool ValueBool => GetValue<bool>();
+
+            /// <summary>
+            /// Retrieve the JToken
+            /// </summary>
+            public JToken JToken => _jToken;
+
+            /// <summary>
+            /// Retrieve the array
+            /// </summary>
+            public IEnumerable<Json> GetArray()
+            {
+                if (_jToken is JArray jArray)
+                {
+                    foreach (var item in jArray)
+                    {
+                        yield return new Json(item);
+                    }
+                }
+                else
+                {
+                    // Return an empty list
+                    // Terminate iteration by returning an empty list
+                    yield break;
+                }
+            }
+
+            /// <summary>
+            /// Retrieve an array cast to the specified type
+            /// </summary>
+            public IEnumerable<T> GetArray<T>()
+            {
+                return GetArray().Select(t => t.GetValue<T>());
+            }
+
+
+            /// <summary>
+            /// Retrieve a dictionary
+            /// </summary>
+            public Dictionary<string, Json> GetDictionary()
+            {
+                if (_jToken is JObject jObject)
+                {
+                    var dictionary = new Dictionary<string, Json>(StringComparer.OrdinalIgnoreCase);
+                    foreach (var property in jObject.Properties())
+                    {
+                        dictionary[property.Name] = new Json(property.Value);
+                    }
+                    return dictionary;
+                }
+                return new Dictionary<string, Json>();
+            }
+        }
+
+        /// <summary>
+        /// Type of value in JSON
+        /// </summary>
+        public enum JsonType
+        {
+            /// <summary>
+            /// Object
+            /// </summary>
+            JObject,
+            /// <summary>
+            /// Array
+            /// </summary>
+            JArray,
+            /// <summary>
+            /// Simple value
+            /// </summary>
+            Value,
+        }
+
+  
+
+}
--- ScheduleDisconnectLight/Program.cs
+++ ScheduleDisconnectLight/Program.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,22 +15,124 @@ namespace ScheduleDisconnectLight
     {
         static void Main(string[] args)
         {
+
+            string stateFile = "state.json";
+
+            // Загружаем состояние (если файла нет, создаётся новое)
+            AppState state = LoadState(stateFile);
+
+            // Увеличиваем счётчик
+            state.Counter++;
+
+            // Ставим текущее время
+            state.LastUpdated = DateTime.Now;
+
+            // Сохраняем обратно
+            SaveState(stateFile, state);
+
+
+
+
+
+            getScheduleYasno();
+
             Console.WriteLine("Отправляем сообщение...");
 
-            try
-            {
-                SendTelegramMessage().GetAwaiter().GetResult();
-                Console.WriteLine("Готово!");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Ошибка: " + ex.Message);
-            }
+            sendTelegramMessage();
         }
 
-        static async Task SendTelegramMessage()
+
+        private static Schedule getScheduleYasno()
         {
-            // Можно брать из переменных среды, а можно пока захардкодить
+            string url = "https://app.yasno.ua/api/blackout-service/public/shutdowns/regions/25/dsos/902/planned-outages";
+
+            string jsonYasnoTmp = "";
+            using (var httpClient = new HttpClient())
+            {
+                try
+                {
+                    // Синхронный GET
+                    HttpResponseMessage response = httpClient.GetAsync(url).Result;
+
+                    // Если ошибка, бросим исключение
+                    response.EnsureSuccessStatusCode();
+
+                    // Читаем тело ответа тоже синхронно
+                    jsonYasnoTmp = response.Content.ReadAsStringAsync().Result;
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ошибка: " + ex.Message);
+                    jsonYasnoTmp = string.Empty;
+                }
+            }
+
+            var jsonYasno = new Json(jsonYasnoTmp)["1.1"];
+            var schedule = new Schedule();
+            schedule.DateUpdate = jsonYasno["updatedOn"].GetValue<DateTime>();
+            if (jsonYasno["today"]["status"].Value == "ScheduleApplies")
+            {
+                schedule.ParamDisconnet1 = new ScheduleTimeDisconnet();
+                schedule.ParamDisconnet1.Date = jsonYasno["today"]["date"].GetValue<DateTime>();
+
+                foreach (var item in jsonYasno["today"]["slots"].GetArray())
+                {
+                    if (item["type"].Value == "Definite")
+                    {
+                        continue;
+                    }
+
+                    double valueStart = item["start"].ValueInt / 60.0;
+                    int hoursStart = (int)valueStart;                    // 8
+                    int minutesStart = (int)((valueStart - hoursStart) * 60); // 0.5 * 60 = 30
+                    var timeStart = new TimeSpan(hoursStart, minutesStart, 0);
+
+
+                    double valueEnd = item["end"].ValueInt / 60.0;
+                    int hoursEnd = (int)valueEnd;                    // 8
+                    int minutesEnd = (int)((valueEnd - hoursEnd) * 60); // 0.5 * 60 = 30
+
+                    var timeEnd = hoursEnd == 24 ? new TimeSpan(23, 59, 0) : new TimeSpan(hoursEnd, minutesEnd, 0);
+
+                    schedule.ParamDisconnet1.PeriodDisconnet.Add(Tuple.Create(timeStart, timeEnd));
+                }
+            }
+            if (jsonYasno["tomorrow"]["status"].Value == "ScheduleApplies")
+            {
+                schedule.ParamDisconnet2 = new ScheduleTimeDisconnet();
+                schedule.ParamDisconnet2.Date = jsonYasno["tomorrow"]["date"].GetValue<DateTime>();
+
+                foreach (var item in jsonYasno["tomorrow"]["slots"].GetArray())
+                {
+                    if (item["type"].Value == "Definite")
+                    {
+                        continue;
+                    }
+
+                    double valueStart = item["start"].ValueInt / 60.0;
+                    int hoursStart = (int)valueStart;                    // 8
+                    int minutesStart = (int)((valueStart - hoursStart) * 60); // 0.5 * 60 = 30
+                    var timeStart = new TimeSpan(hoursStart, minutesStart, 0);
+
+
+                    double valueEnd = item["end"].ValueInt / 60.0;
+                    int hoursEnd = (int)valueEnd;                    // 8
+                    int minutesEnd = (int)((valueEnd - hoursEnd) * 60); // 0.5 * 60 = 30
+
+                    var timeEnd = hoursEnd == 24 ? new TimeSpan(23, 59, 0) : new TimeSpan(hoursEnd, minutesEnd, 0);
+
+                    schedule.ParamDisconnet2.PeriodDisconnet.Add(Tuple.Create(timeStart, timeEnd));
+                }
+            }
+            return schedule;
+
+
+        }
+
+
+        public static void sendTelegramMessage()
+        {
             string botToken = "7911836999:AAHeC6qjw-Kis9xwA332YTq2ns1YI1AMdMI";
             string chatId = "-1002275491172";
 
@@ -43,17 +149,76 @@ namespace ScheduleDisconnectLight
                 string url = $"https://api.telegram.org/bot{botToken}/sendMessage";
 
                 var data = new Dictionary<string, string>
-                {
-                    { "chat_id", chatId },
-                    { "text", text }
-                };
+                    {
+                        { "chat_id", chatId },
+                        { "text", text }
+                    };
 
                 using (var content = new FormUrlEncodedContent(data))
                 {
-                    var response = await httpClient.PostAsync(url, content);
+                    // Синхронный POST
+                    HttpResponseMessage response = httpClient.PostAsync(url, content).Result;
+
+                    // Бросит исключение, если статус не 2xx
                     response.EnsureSuccessStatusCode();
                 }
             }
         }
+
+        // Считывание state.json
+        static AppState LoadState(string path)
+        {
+            if (!File.Exists(path))
+            {
+                return new AppState
+                {
+                    LastUpdated = DateTime.Now,
+                    Counter = 0
+                };
+            }
+
+            string json = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<AppState>(json);
+        }
+
+        // Сохранение state.json
+        static void SaveState(string path, AppState state)
+        {
+            string json = JsonConvert.SerializeObject(state, Formatting.Indented);
+            File.WriteAllText(path, json);
+        }
+
     }
+
+    // Класс "мини-базы"
+    class AppState
+    {
+        public DateTime LastUpdated { get; set; }
+        public int Counter { get; set; }
+    }
+
+    public class Schedule
+    {
+        public DateTime DateUpdate;
+        public ScheduleTimeDisconnet ParamDisconnet1;
+        public ScheduleTimeDisconnet ParamDisconnet2;
+        public Schedule()
+        {
+           
+        }
+    }
+
+    public class ScheduleTimeDisconnet
+    {
+        public DateTime Date;
+        public List<Tuple<TimeSpan,TimeSpan>> PeriodDisconnet;
+
+        public ScheduleTimeDisconnet()
+        {
+            PeriodDisconnet = new List<Tuple<TimeSpan, TimeSpan>>();
+        }
+    }
+
+
+
 }
